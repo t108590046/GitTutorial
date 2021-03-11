ae6a95f9628a73fe0bbffb7a97cc49a537253000
LAPTOP-638SSQ21\banan <t108590046@ntut.org.tw>
2021-03-11 21:10:02 +0800

    練習八甲

--- GitTutorial.cpp
+++ GitTutorial.cpp
@@ -14,22 +14,22 @@ bool Guess(int number) {
 
 		target = rand()%100+1;
 	}
-	if (number == target){
-		std::cout<<"Correct !!";
-		target = -1;
-		return true;
-	}
-	else std::cout<<"Wrong" << std::endl;
+	if(number >target){
+		std::cout<<"Smaller"<<std::endl;
 		return false;
 	}
-
+	else if(number < target){
+		std::cout<<"Bigger"<<std::endl;
+		return false;
+	}
+return true;
 }
 
 int main()
 {
 	int guess;
 	do {
-		
+		std::cout<<"Choose a number between 1-100:"
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
