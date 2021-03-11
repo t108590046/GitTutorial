58ab9adc2e864233959b4b3cb2729d08c131a653
MSI\leo89 <leo89125@gmail.com>
2021-03-11 21:17:12 +0800

    練習8 乙

--- GitTutorial.cpp
+++ GitTutorial.cpp
@@ -14,22 +14,21 @@ bool Guess(int number) {
 
 		target = rand()%100+1;
 	}
-	if(number >target){
-		std::cout<<"Smaller"<<std::endl;
+	if(number == target){
+		std::cout<<"Correct !!"<<std::endl;
+		target = -1;
+		return true;
+	}
+	else std::cout<<"Wrong"<< std::endl;
 		return false;
 	}
-	else if(number < target){
-		std::cout<<"Bigger"<<std::endl;
-		return false;
-	}
-return true;
-}
+
 
 int main()
 {
 	int guess;
 	do {
-		std::cout<<"Choose a number between 1-100:"
+	
 		std::cin >> guess;
 	} while (!Guess(guess));
 	return 0;
